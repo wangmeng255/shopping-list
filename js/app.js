cdd5b07fd934a1322e80bd11590efe0f91a64427
wangmeng <wangmeng255@gmail.com>
2016-05-19 11:11:09 -0700

    add button

--- js/app.js
+++ js/app.js
@@ -1,19 +1,20 @@
 $(document).ready( function() {
+	var i_need_sum = 0;
 	$("#add").click( function() {
 		//console.log("button click");
 		var item = [$("#item").val(), parseInt($("#qty").val()), parseFloat($("#price").val())];
 		if(item[0] && item[1] && item[2])
 		{
-			addTableRow($("#t-I-need"), item);
+			addTableRow($("#t-I-need"), item, $("#I-need-total"));
 		}
 		else
 		{
 			alert("Please input item, qty and price. ^_^");
 		}
 	});
-	function addTableRow(jQtable, item){
-    jQtable.each(function(){
-        var $table = $(this);
+	function addTableRow(jQtable, item, jQtotal){
+    //jQtable.each(function(){
+    	//var $table = $(this);
         // Number of th's in the last table row
         var ths = "<tr>";
         var total = item[1]*item[2];
@@ -21,14 +22,16 @@ $(document).ready( function() {
             ths += "<th>"+item[i]+"</th>";
         }
         ths += "<th>$"+String(total)+"</th>";
-        ths += "<td><img class='check' src='./img/f-check.png' width='26' height='26'><img class='cross' src='./img/f-cross.png' width='23' height='23'></td>";
+        ths += "<td><img class='check' src='./img/f-check.png'><img class='cross' src='./img/f-cross.png'></td>";
         ths += "</tr>";
-        if($("tbody", this).length > 0){
-            $("tbody", this).append(ths);
+        if($("tbody", jQtable).length > 0){
+            $("tbody", jQtable).append(ths);
         }else {
-            $(this).append(ths);
+            $(jQtable).append(ths);
         }
-    });
+        i_need_sum += total;
+		jQtotal.text("Total: $ "+String(i_need_sum));
+    //});
 	}
 	$(".check").click(function() {
     	
